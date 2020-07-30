eadad7f107a5835f22cd92214eb1cfcf61eaefae
Andreas Franck <a.franck@soton.ac.uk>
2020-07-30 22:02:52 +0100

    Made unit tests independent of the location of mysofa2json.
    * Works with multi-configuration generators (e.g, Xcode or Visual Studio)
    * Works with out-of-source builds.

--- tests/compare.sh
+++ tests/compare.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 SCRIPTDIR=${0%/*}
+
+MYSOFA_BINARY=$2
 NODEJS=$(which node nodejs false | head -1)
 
-${MYSOFA2JSON:-${SCRIPTDIR}/../build/src/mysofa2json} -c -s "$1".sofa >tmp1.json 2>tmp1.txt
+${MYSOFA_BINARY} -c -s "$1".sofa >tmp1.json 2>tmp1.txt
 
 ret=$?
-if [ "$ret" != 0 ]; then 
+if [ "$ret" != 0 ]; then
     cat tmp1.txt
     echo Error libmysofa $ret
     exit $ret
@@ -16,10 +18,9 @@ cp -f "${SCRIPTDIR}/json-diff.js" . 2>/dev/null || true
 bunzip2 -c -k "$1".json.bz2 >./tmp2.json
 "${NODEJS}" ./json-diff.js ./tmp1.json ./tmp2.json
 ret=$?
-if [ "$ret" != 0 ]; then 
+if [ "$ret" != 0 ]; then
     echo Diff $ret
     exit $ret
 fi
 echo ok
 rm tmp1.json tmp1.txt tmp2.json
-
